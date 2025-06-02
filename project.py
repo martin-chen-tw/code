3dde94641f28bbfd99a9cb718d90269ffae01559
martin.chen <martin.chen@f60375fb-89c3-4d23-96e1-5216dc9c0f52>
2025-06-02 08:57:10 +0000

    git-svn-id: svn://125.227.14.31/code@2 f60375fb-89c3-4d23-96e1-5216dc9c0f52

---
+++ project.py
@@ -0,0 +1,26 @@
+import random
+import tkinter as tk
+#初始化
+window = tk.Tk()
+window.title("This is a game manually created by MING-HAO CHEN")
+window.geometry("800x600")
+window.resizable("false","false")
+window.mainloop()
+
+class Snack:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
